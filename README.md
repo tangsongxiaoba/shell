58771bb7fe52a4c55bf629ada828ea0885950a6c
tangsongxiaoba <tangsongxiaoba@163.com>
2025-05-07 00:48:50 +0800

    third version complete

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ TASH (TAngsongxiaoba SHell), a simple shell for my UNIX homework.
 
 - 2025.04.18: first version implemented, from [the classic start](https://github.com/brenns10/lsh/blob/master/src/main.c).
 - 2025.05.07: second version implemented, support I/O redirect('>', '<', '>>'), support pipeline('|').
+- 2025.05.07: third version implemented, add highlight support for prompts and errors.
--- tash.c
+++ tash.c
@@ -28,31 +28,41 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define PROMPT "tash> "
+#define PROMPT KGRN "tash> " KNRM
 #define BUF_INC_SIZE 128
 #define DELIM " \t\r\n"
 #define STATUS_OK 1
 #define STATUS_END 0
 
-#define GETLINE_FAILED ":( tash: get new line failed"
-#define MEM_ALLOC_FAILED ":( tash: mem alloc failed"
-#define EXEC_FAILED ":( tash: exec prog failed"
-#define FORK_FAILED ":( tash: fork failed"
-#define SYNTAX_ERROR_NEWLINE_LT ":( tash: syntax error near unexpected token `newline' for `<'"
-#define SYNTAX_ERROR_NEWLINE_GT ":( tash: syntax error near unexpected token `newline' for `>'"
-#define SYNTAX_ERROR_NEWLINE_GTGT ":( tash: syntax error near unexpected token `newline' for `>>'"
-#define TOO_MANY_ARGS ":( tash: too many arguments for a command segment"
-#define OPEN_INPUT_FAILED ":( tash: open input file"
-#define DUP2_STDIN_FAILED ":( tash: dup2 stdin failed"
-#define OPEN_OUTPUT_FAILED ":( tash: open output file"
-#define DUP2_STDOUT_FAILED ":( tash: dup2 stdout failed"
-#define CD_MISSING_ARG ":( tash: cd: missing argument"
-#define CD_FAILED ":( tash: cd"
-#define SYNTAX_ERROR_PIPE ":( tash: syntax error near `|'"
-#define TOO_MANY_CMDS_PIPE ":( tash: too many commands in pipeline"
-#define PIPE_FAILED ":( tash: pipe failed"
-#define CHILD_DUP2_STDIN_FAILED ":( tash: child dup2 stdin failed"
-#define CHILD_DUP2_STDOUT_FAILED ":( tash: child dup2 stdout failed"
+#define GETLINE_FAILED KRED ":( tash: get new line failed" KNRM
+#define MEM_ALLOC_FAILED KRED ":( tash: mem alloc failed" KNRM
+#define EXEC_FAILED KRED ":( tash: exec prog failed" KNRM
+#define FORK_FAILED KRED ":( tash: fork failed" KNRM
+#define SYNTAX_ERROR_NEWLINE_LT KRED ":( tash: syntax error near unexpected token `newline' for `<'" KNRM
+#define SYNTAX_ERROR_NEWLINE_GT KRED ":( tash: syntax error near unexpected token `newline' for `>'" KNRM
+#define SYNTAX_ERROR_NEWLINE_GTGT KRED ":( tash: syntax error near unexpected token `newline' for `>>'" KNRM
+#define TOO_MANY_ARGS KRED ":( tash: too many arguments for a command segment" KNRM
+#define OPEN_INPUT_FAILED KRED ":( tash: open input file" KNRM
+#define DUP2_STDIN_FAILED KRED ":( tash: dup2 stdin failed" KNRM
+#define OPEN_OUTPUT_FAILED KRED ":( tash: open output file" KNRM
+#define DUP2_STDOUT_FAILED KRED ":( tash: dup2 stdout failed" KNRM
+#define CD_MISSING_ARG KRED ":( tash: cd: missing argument" KNRM
+#define CD_FAILED KRED ":( tash: cd" KNRM
+#define SYNTAX_ERROR_PIPE KRED ":( tash: syntax error near `|'" KNRM
+#define TOO_MANY_CMDS_PIPE KRED ":( tash: too many commands in pipeline" KNRM
+#define PIPE_FAILED KRED ":( tash: pipe failed" KNRM
+#define CHILD_DUP2_STDIN_FAILED KRED ":( tash: child dup2 stdin failed" KNRM
+#define CHILD_DUP2_STDOUT_FAILED KRED ":( tash: child dup2 stdout failed" KNRM
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define KBLD  "\x1B[1m"
 
 char *tash_read();
 char **tash_split(char *);
@@ -61,20 +71,20 @@ void tash_loop();
 void tash_prepare_exec(char **);
 
 int main() {
-    printf("-----------------------------------------------------------\n");
-    printf("      ___           ___           ___           ___     \n");
-    printf("     /\\  \\         /\\  \\         /\\  \\         /\\__\\    \n");
-    printf("     \\:\\  \\       /::\\  \\       /::\\  \\       /:/  /    \n");
-    printf("      \\:\\  \\     /:/\\:\\  \\     /:/\\ \\  \\     /:/__/     \n");
-    printf("      /::\\  \\   /::\\~\\:\\  \\   _\\:\\~\\ \\  \\   /::\\  \\ ___ \n");
-    printf("     /:/\\:\\__\\ /:/\\:\\ \\:\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\  /\\__\\\n");
-    printf("    /:/  \\/__/ \\/__\\:\\/:/  / \\:\\ \\:\\ \\/__/ \\/__\\:\\/:/  /\n");
-    printf("   /:/  /           \\::/  /   \\:\\ \\:\\__\\        \\::/  / \n");
-    printf("   \\/__/            /:/  /     \\:\\/:/  /        /:/  /  \n");
-    printf("                   /:/  /       \\::/  /        /:/  /   \n");
-    printf("                   \\/__/         \\/__/         \\/__/    \n");
-    printf("\n-----------------------------------------------------------\n\n");
-    printf("Welcome to use TASH (TAngsongxiaoba SHell)!\n\n");
+    printf("%s-----------------------------------------------------------%s\n", KCYN, KNRM);
+    printf("%s      ___           ___           ___           ___     %s\n", KBLU, KNRM);
+    printf("%s     /\\  \\         /\\  \\         /\\  \\         /\\__\\    %s\n", KBLU, KNRM);
+    printf("%s     \\:\\  \\       /::\\  \\       /::\\  \\       /:/  /    %s\n", KBLU, KNRM);
+    printf("%s      \\:\\  \\     /:/\\:\\  \\     /:/\\ \\  \\     /:/__/     %s\n", KBLU, KNRM);
+    printf("%s      /::\\  \\   /::\\~\\:\\  \\   _\\:\\~\\ \\  \\   /::\\  \\ ___ %s\n", KBLU, KNRM);
+    printf("%s     /:/\\:\\__\\ /:/\\:\\ \\:\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\  /\\__\\%s\n", KBLU, KNRM);
+    printf("%s    /:/  \\/__/ \\/__\\:\\/:/  / \\:\\ \\:\\ \\/__/ \\/__\\:\\/:/  /%s\n", KBLU, KNRM);
+    printf("%s   /:/  /           \\::/  /   \\:\\ \\:\\__\\        \\::/  / %s\n", KBLU, KNRM);
+    printf("%s   \\/__/            /:/  /     \\:\\/:/  /        /:/  /  %s\n", KBLU, KNRM);
+    printf("%s                   /:/  /       \\::/  /        /:/  /   %s\n", KBLU, KNRM);
+    printf("%s                   \\/__/         \\/__/         \\/__/    %s\n", KBLU, KNRM);
+    printf("\n%s-----------------------------------------------------------%s\n\n", KCYN, KNRM);
+    printf("%sWelcome to use TASH (TAngsongxiaoba SHell)!%s\n\n", KYEL KBLD, KNRM);
     tash_loop();
     return 0;
 }
@@ -87,7 +97,8 @@ char *tash_read() {
             printf("\n");
             exit(EXIT_SUCCESS);
         } else {
-            perror(GETLINE_FAILED);
+            fprintf(stderr, "%s: ", GETLINE_FAILED);
+            perror(NULL);
             exit(EXIT_FAILURE);
         }
     }
@@ -104,7 +115,8 @@ char **tash_split(char *line) {
 
     char **tokens = (char **)malloc(n * sizeof(char *));
     if (tokens == NULL) {
-        perror(MEM_ALLOC_FAILED);
+        fprintf(stderr, "%s: ", MEM_ALLOC_FAILED);
+        perror(NULL);
         exit(EXIT_FAILURE);
     }
 
@@ -115,7 +127,8 @@ char **tash_split(char *line) {
             n += BUF_INC_SIZE;
             tokens = realloc(tokens, n * sizeof(char *));
             if (tokens == NULL) {
-                perror(MEM_ALLOC_FAILED);
+                fprintf(stderr, "%s: ", MEM_ALLOC_FAILED);
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
         }
@@ -193,11 +206,13 @@ void tash_prepare_exec(char **args) {
     if (input_file) {
         int fd_in = open(input_file, O_RDONLY);
         if (fd_in == -1) {
-            perror(OPEN_INPUT_FAILED);
+            fprintf(stderr, "%s: ", OPEN_INPUT_FAILED);
+            perror(NULL);
             exit(EXIT_FAILURE);
         }
         if (dup2(fd_in, STDIN_FILENO) == -1) {
-            perror(DUP2_STDIN_FAILED);
+            fprintf(stderr, "%s: ", DUP2_STDIN_FAILED);
+            perror(NULL);
             exit(EXIT_FAILURE);
         }
         close(fd_in);
@@ -212,11 +227,13 @@ void tash_prepare_exec(char **args) {
         }
         int fd_out = open(output_file, flags, 0644);
         if (fd_out == -1) {
-            perror(OPEN_OUTPUT_FAILED);
+            fprintf(stderr, "%s: ", OPEN_OUTPUT_FAILED);
+            perror(NULL);
             exit(EXIT_FAILURE);
         }
         if (dup2(fd_out, STDOUT_FILENO) == -1) {
-            perror(DUP2_STDOUT_FAILED);
+            fprintf(stderr, "%s: ", DUP2_STDOUT_FAILED);
+            perror(NULL);
             exit(EXIT_FAILURE);
         }
         close(fd_out);
@@ -227,7 +244,8 @@ void tash_prepare_exec(char **args) {
     }
 
     if (execvp(clean_args[0], clean_args) == -1) {
-        perror(EXEC_FAILED);
+        fprintf(stderr, "%s: ", EXEC_FAILED);
+        perror(NULL);
         exit(EXIT_FAILURE); 
     }
 }
@@ -245,7 +263,8 @@ int tash_exec(char **args) {
             fprintf(stderr, "%s\n", CD_MISSING_ARG);
         } else {
             if (chdir(args[1]) != 0) {
-                perror(CD_FAILED);
+                fprintf(stderr, "%s: ", CD_FAILED);
+                perror(NULL);
             }
         }
         return STATUS_OK;
@@ -278,7 +297,8 @@ int tash_exec(char **args) {
 
         if (i < num_cmds - 1) {
             if (pipe(pipe_fds) == -1) {
-                perror(PIPE_FAILED);
+                fprintf(stderr, "%s: ", PIPE_FAILED);
+                perror(NULL);
                 if (in_fd != STDIN_FILENO) close(in_fd);
                 for(int j = 0; j < i; j++) waitpid(pids[j], NULL, 0);
                 return STATUS_OK; 
@@ -287,7 +307,8 @@ int tash_exec(char **args) {
 
         pids[i] = fork();
         if (pids[i] == -1) {
-            perror(FORK_FAILED);
+            fprintf(stderr, "%s: ", FORK_FAILED);
+            perror(NULL);
             if (in_fd != STDIN_FILENO) close(in_fd);
             if (i < num_cmds - 1) {
                 close(pipe_fds[0]); 
@@ -300,7 +321,8 @@ int tash_exec(char **args) {
         if (pids[i] == 0) {
             if (in_fd != STDIN_FILENO) {
                 if (dup2(in_fd, STDIN_FILENO) == -1) {
-                    perror(CHILD_DUP2_STDIN_FAILED); 
+                    fprintf(stderr, "%s: ", CHILD_DUP2_STDIN_FAILED);
+                    perror(NULL);
                     exit(EXIT_FAILURE);
                 }
                 close(in_fd);
@@ -309,7 +331,8 @@ int tash_exec(char **args) {
             if (i < num_cmds - 1) {
                 close(pipe_fds[0]);
                 if (dup2(pipe_fds[1], STDOUT_FILENO) == -1) {
-                    perror(CHILD_DUP2_STDOUT_FAILED);
+                    fprintf(stderr, "%s: ", CHILD_DUP2_STDOUT_FAILED);
+                    perror(NULL);
                     exit(EXIT_FAILURE);
                 }
                 close(pipe_fds[1]);
